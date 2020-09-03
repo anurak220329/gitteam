d815baf7ec093dee4f21343029ec54a85e61c1bf
anurak <604259014@webmail.npru.ac.th>
2020-09-03 15:56:44 +0700

    Use Class

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Calculator{
 	public int add(int num1,int num2){
 		return num1+num2;
 }
-public int mutiply(int num1,int num2){
+public int multiply(int num1,int num2){
 		return num1*num2;
 }
 public int subtract(int num1,int num2){
